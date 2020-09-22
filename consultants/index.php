9a00793c18b940a5c69941e496cb85748d2e93ca
dkowsikpai <dkowsikpai@gmail.com>
2020-09-22 09:31:49 +0530

    js-grid

--- consultants/index.php
+++ consultants/index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <?php
-    session_start();
+session_start();
 
-    if (!isset($_SESSION["login"])){
-        header("Location: ../");
-    }
-
-    // include('../config/connection.php');
-    // connection();
+// if (!isset($_SESSION["login"])){
+//     header("Location: ../");
+// }
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/css/menu.css" rel="stylesheet" type="text/css"/>
-    <title>Open PPM | Dashboard</title>
+    <link href="../assets/css/jsGrid/jsgrid.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/css/jsGrid/jsgrid-theme.css" rel="stylesheet" type="text/css"/>
+    <title>Open PPM | Consultants</title>
 </head>
 <body>
-    <header class="menu">
+<header class="menu">
         <span class="menu-title">
             Open PPM
         </span>
@@ -47,10 +46,15 @@
           </div>          
     </header>
     <main>
-        Hi 
-        <?php
-            echo $_SESSION["userType"];
-        ?>
+        <div class="current-nav">
+            Open PPM / Consultants
+        </div>
+        <div class="grid">
+            <div id="jsGrid"></div>
+        </div>
     </main>
 </body>
+<script src="../assets/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/jsGrid/jsgrid.js" type="text/javascript"></script>
+<script src="../assets/js/consultants.js" type="text/javascript"></script>
 </html>
