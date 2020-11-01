69e5fc4f80f1de4c3ab4bb7709fd1ee9e102e0e7
dkowsikpai <dkowsikpai@gmail.com>
2020-11-01 10:07:20 +0530

    Dashboard Responsive Grid

--- dashboard/index.php
+++ dashboard/index.php
@@ -16,6 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/css/menu.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/css/dashboard.css" rel="stylesheet" type="text/css"/>
     <title>Open PPM | Dashboard</title>
 </head>
 <body>
@@ -24,10 +25,31 @@
         navBar();
     ?>
     <main>
-        Hi 
-        <?php
-            echo $_SESSION["userType"];
-        ?>
+        <div class="chart-frame">
+            <div class="chart-row">
+                <div class="chart-panel">
+                    1
+                </div>
+                <div class="chart-panel">
+                    2
+                </div>
+                <div class="chart-panel">
+                    3
+                </div>
+            </div>
+            <div class="chart-row">
+                <div class="chart-panel">
+                    4
+                </div>
+                <div class="chart-panel">
+                    5
+                </div>
+                <div class="chart-panel">
+                    6
+                </div>
+            </div>
+        </div>
+        
     </main>
 </body>
 </html>
