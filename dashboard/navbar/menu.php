1b0f02bf797e6cd930168995afccb0430cb8d5e2
dkowsikpai <dkowsikpai@gmail.com>
2020-11-04 09:52:48 +0530

    Menubar Responsive

--- dashboard/navbar/menu.php
+++ dashboard/navbar/menu.php
@@ -2,21 +2,22 @@
 function navBar (){
 ?>
     <header class="menu">
-        <span class="menu-title">
+        <a href="../dashboard/"><span class="menu-title">
             Open PPM
-        </span>
-        <span class="menu-item">
+        </span></a>
+        <a href="../dashboard/"><span class="menu-item">
             Dashboard
-        </span>
+        </span></a>
         <a href="../consultants/"><span class="menu-item">
             Consultants
         </span></a>
         <a href="../customers/"><span class="menu-item">
             Customers
         </span></a>
-        <span class="menu-item">
+        <!-- Change the href -->
+        <a href="../customers/"><span class="menu-item"> 
             Projects
-        </span>
+        </span></a>
         <div class="dropdown">
             <button class="dropbtn">
                 <span class="menu-avatar">
@@ -30,6 +31,39 @@ function navBar (){
             </div>
           </div>          
     </header>
+    <div class="mob-menu">
+        <a href="../dashboard/"><span class="mob-menu-item">
+            <div class="mob-menu-item-img">
+                <img src="../assets/images/Menu/dashboard.png" alt="Dashboard" class="mob-menu-img"/>
+                <p class="mob-menu-item-subtitle">Dashboard</p>
+            </div>
+        </span></a>
+        <a href="../consultants/"><span class="mob-menu-item">
+            <div class="mob-menu-item-img">
+                <img src="../assets/images/Menu/consultants.png" alt="Consultants" class="mob-menu-img"/>
+                <p class="mob-menu-item-subtitle">Consultants</p>
+            </div>
+        </span></a>
+        <a href="../customers/"><span class="mob-menu-item">
+            <div class="mob-menu-item-img">
+                <img src="../assets/images/Menu/customers.png" alt="Customers" class="mob-menu-img"/>
+                <p class="mob-menu-item-subtitle">Customers</p>
+            </div>
+        </span></a>
+        <!-- change href -->
+        <a href="../customers/"><span class="mob-menu-item">
+            <div class="mob-menu-item-img">
+                <img src="../assets/images/Menu/projects.png" alt="Projects" class="mob-menu-img"/>
+                <p class="mob-menu-item-subtitle">Projects</p>
+            </div>
+        </span></a>
+        <a href="../logout/"><span class="mob-menu-item">
+            <div class="mob-menu-item-img">
+                <img src="../assets/images/Menu/logout.png" alt="Logout" class="mob-menu-img"/>
+                <p class="mob-menu-item-subtitle">Logout</p>
+            </div>
+        </span></a>
+    </div>
 <?php
 }
 ?>
